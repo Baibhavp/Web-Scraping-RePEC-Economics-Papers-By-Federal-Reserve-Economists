3922dd59114a76d3ec5e04c3f371a074291b6d18
Baibhav Paudel <baibhav.paudel@thamescollege.edu.np>
2025-11-19 11:20:10 +0545

    extract dates

--- fed.py
+++ fed.py
@@ -10,10 +10,14 @@ soup = BeautifulSoup(page.text, 'html.parser')
 results = soup.find_all('div', class_='col-xs-12 col-md-12 col-sm-12')
 #print(results)
 
+'''
+for p in results:
+    title = p.find_all('h5')
+    title_records = [t.get_text() for t in title[2:]]
+'''
 
 for p in results:
-    titles = p.find_all('h5')
-    records = [t.get_text() for t in titles[2:]]
+    date = p.find_all('time')
+    date_records = [t.get_text().strip() for t in date]
 
-print(records)
 
