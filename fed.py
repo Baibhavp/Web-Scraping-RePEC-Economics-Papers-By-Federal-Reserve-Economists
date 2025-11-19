7b3c417c604ac05785e25ab04e765faa397d6439
Baibhav Paudel <baibhav.paudel@thamescollege.edu.np>
2025-11-19 11:24:43 +0545

    extract fed numbers

--- fed.py
+++ fed.py
@@ -15,9 +15,15 @@ for p in results:
     title = p.find_all('h5')
     title_records = [t.get_text() for t in title[2:]]
 '''
-
+'''
 for p in results:
     date = p.find_all('time')
     date_records = [t.get_text().strip() for t in date]
+'''
+'''
+for p in results:
+    feds_number = p.find_all('span', class_='badge badge--feds')
+    feds_number_records = [t.get_text().strip() for t in feds_number]
 
-
+print(feds_number_records)
+'''
