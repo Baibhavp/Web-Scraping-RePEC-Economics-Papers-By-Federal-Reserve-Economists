c446c96fd40f280857e9e79590ac3072d358fbb1
Baibhav Paudel <baibhav.paudel@thamescollege.edu.np>
2025-11-19 14:13:47 +0545

    jel code extractor

--- fed.py
+++ fed.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from extraction import ExtractInfo
 
+
 url = 'https://www.federalreserve.gov/econres/feds/2025.htm'
 
 page = requests.get(url)
@@ -14,19 +15,3 @@ soup = BeautifulSoup(page.text, 'html.parser')
 results = soup.find_all('div', class_='col-xs-12 col-md-12 col-sm-12')
 
 extract = ExtractInfo(results)
-
-doi_links = extract.get_doi_link(results)
-
-
-# Testing to extract jel codes
-doi_url = doi_links[1]
-
-r = requests.get(doi_url)
-doi_page = BeautifulSoup(r.text, 'html.parser')
-
-pdf_url = doi_page.find('a', string="Full Paper")['href']
-
-full_pdf_link = "https://www.federalreserve.gov" + pdf_url
-print(full_pdf_link)
-
-
